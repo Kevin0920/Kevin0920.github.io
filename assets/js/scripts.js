359252555f6ef33beddb8cd1143998f1180f1d3d
Kevin0920 <kevinlingsike@gmail.com>
2017-12-01 17:38:08 -0800

    updated map

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,6 @@
 jQuery(function ($) {
 
     'use strict';
-
     $(window).ready(function() {
         $('#pre-status').fadeOut();
         $('#tt-preloader').delay(350).fadeOut('slow');
@@ -224,6 +223,70 @@ jQuery(function ($) {
     // Google Map
     // -------------------------------------------------------------
 
+    (function () {
+        var myLatlng = new google.maps.LatLng(37.724930, -122.156077);
+
+            var styles = [
+                {
+                    featureType: "landscape",
+                    stylers: [
+                        { color: '#f7f7f7' }
+                    ]
+                },{
+                    featureType: "natural",
+                    stylers: [
+                        { hue: '#00ffe6' }
+                    ]
+                },{
+                    featureType: "road",
+                    stylers: [
+                        { hue: '#fff' },
+                        { saturation: -70 }
+                    ]
+                },{
+                    featureType: "building",
+                    elementType: "labels",
+                    stylers: [
+                        { hue: '' }
+                    ]
+                },{
+                    featureType: "poi", //points of interest
+                    stylers: [
+                        { hue: '' }
+                    ]
+                }
+            ];
+
+            var mapOptions = {
+                zoom: 15,
+                scrollwheel: false,
+                center: myLatlng,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                disableDefaultUI: true,
+                styles: styles
+            }
+            var map = new google.maps.Map(document.getElementById('mapCanvas'), mapOptions);
+
+            var marker = new google.maps.Marker({
+                position: myLatlng,
+                map: map,
+                animation: google.maps.Animation.DROP,
+                title: 'Hello I am here!'
+            });
+
+            var contentString = '' +
+                    '' +
+                    '';
+
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+
+            google.maps.event.addListener(marker, 'click', function () {
+                infowindow.open(map, marker);
+            });
+
+    }());
 
 
 });
